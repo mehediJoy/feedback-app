4eb0460a5f5ca68c3792e42ac22920150c32b822
mehediJoy <mhjoy1998@gmail.com>
2022-05-30 22:53:30 +0600

    Added Event Handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeebackData from './data/FeedbackData'
+
 function App() {
-  return <h1>This is App component.</h1>;
+  const [feedback, setFeedback] = useState(FeebackData)
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App

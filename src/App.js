ae6a772f58bb4a08b33b541810cb04bcf0290391
mehediJoy <mhjoy1998@gmail.com>
2022-06-06 13:51:08 +0600

    Added Router and About Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import FeebackData from './data/FeedbackData'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
 
 function App() {
   const [feedback, setFeedback] = useState(FeebackData)
@@ -18,14 +21,26 @@ function App() {
     }
   }
   return (
-    <>
-      <Header />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback}/>
-        <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
-    </>
+    <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback}/>
+                  <FeedbackStats feedback={feedback}/>
+                  <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                  <AboutIconLink/>
+                </>
+              }
+            ></Route>
+
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+        </div>
+      </Router>
   )
 }
 
